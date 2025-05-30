ea89e324297fbc1b98488499398f1d822feb030f
Berenice Madrid <madridberenice21@gmail.com>
2025-05-29 20:58:48 -0600

    Update firebase-config.js
    Configuración de SDK de Firebase

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,22 @@
-// Reemplaza con tu configuración real de Firebase
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_PROYECTO.firebaseapp.com",
-  databaseURL: "https://TU_PROYECTO.firebaseio.com",
-  projectId: "TU_PROYECTO",
-  storageBucket: "TU_PROYECTO.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "1:XXXX:web:XXXX"
+  apiKey: "AIzaSyC0zsLLOXJ3atSBqHeQaACT3hrpabXC9CM",
+  authDomain: "logincorreo-9d4c9.firebaseapp.com",
+  databaseURL: "https://logincorreo-9d4c9-default-rtdb.firebaseio.com",
+  projectId: "logincorreo-9d4c9",
+  storageBucket: "logincorreo-9d4c9.firebasestorage.app",
+  messagingSenderId: "202517555750",
+  appId: "1:202517555750:web:33a83f04a3ab94497014e7"
 };
 
-firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+
 
