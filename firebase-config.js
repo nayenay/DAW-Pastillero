ebb5f61300e8eac55c85cd5090068f4c7c789805
Berenice Madrid <madridberenice21@gmail.com>
2025-05-29 20:47:51 -0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,13 @@
+// Reemplaza con tu configuración real de Firebase
+const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_PROYECTO.firebaseapp.com",
+  databaseURL: "https://TU_PROYECTO.firebaseio.com",
+  projectId: "TU_PROYECTO",
+  storageBucket: "TU_PROYECTO.appspot.com",
+  messagingSenderId: "XXXX",
+  appId: "1:XXXX:web:XXXX"
+};
+
+firebase.initializeApp(firebaseConfig);
 
