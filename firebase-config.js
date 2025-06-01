33de5eaec20805edf0d5b23eb2f8523542b41452
Berenice Madrid <madridberenice21@gmail.com>
2025-05-31 21:57:11 -0600

    Update firebase-config.js
    Para corregir el bug de login, aparecicia este error:
    Uncaught TypeError: Failed to resolve module specifier "firebase/app". Relative references must start with either "/", "./", or "../".
    
    aparentemente es por la versión que estoy manejando, v9.09 creo

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,15 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-import { getAuth } from "firebase/auth";
-import { getDatabase } from "firebase/database";
+// CAMBIAR ESTAS LÍNEAS:
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// import { getDatabase } from "firebase/database";
 
-// Your web app's Firebase configuration
+// POR ESTAS LÍNEAS CON LAS URLs DEL CDN:
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js';
+import { getAuth } from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js';
+import { getDatabase } from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js';
+
+// Tu configuración existente
 const firebaseConfig = {
   apiKey: "AIzaSyC0zsLLOXJ3atSBqHeQaACT3hrpabXC9CM",
   authDomain: "logincorreo-9d4c9.firebaseapp.com",
@@ -22,6 +26,3 @@ const auth = getAuth(app);
 const db = getDatabase(app);
 
 export { auth, db };
-
-
-
